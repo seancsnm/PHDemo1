23b9b0ea78109914996eef47e89735c284c1995b
Benjamin Bean <benjamin.bean@comcast.net>
2017-07-05 01:09:57 -0600

    fixed minor bug

--- GPIO/main.cpp
+++ GPIO/main.cpp
@@ -16,20 +16,20 @@ int main()
 		cout << "(currently just the file /root/start.txt)" << endl;
 
 		int startVal = 0;
-		while (startVal == 0)
+		while ((startVal = readNumberFromFile("/root/start.txt")) == 0)
 		{
-			startVal = readNumberFromFile("/root/start.txt");
 			this_thread::sleep_for(chrono::milliseconds(100));
 		}
 
 		int endVal = 0;
-		while (endVal == 0)
+		while ((endVal = readNumberFromFile("/root/end.txt")) == 0)
 		{
-			endVal = readNumberFromFile("/root/end.txt");
 			int analogValue0 = readAnalog(0);
 			cout << analogValue0 << endl;
 			this_thread::sleep_for(chrono::milliseconds(100));
 		}
+
+		this_thread::sleep_for(chrono::milliseconds(200));
 	}
 
 	return 0;
